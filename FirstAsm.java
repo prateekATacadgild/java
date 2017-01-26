d0d420a38d3bcc01801851cadf7d4fab22c88e82
Akshyanshu <aksbarik.oraunx@gmail.com>
2017-01-26 15:40:00 +0530

    Add files via upload

---
+++ FirstAsm.java
@@ -0,0 +1,65 @@
+import java.math.BigInteger;
+public class FirstAsm {
+	int a=20;
+	int b=10;
+	int ans;
+	int c,d,e;
+    int asc ;
+    String ch;
+
+//Assignment 1.1	
+  void add(int a,int b)
+  {
+	  System.out.println("Assignment 1.1");
+	  BigInteger b1=new BigInteger(new Integer(a).toString());
+	  BigInteger b2=new BigInteger(new Integer(b).toString());
+	  BigInteger sum=b1.add(b2);
+     System.out.println("Sum is " +sum);
+  }
+
+void add1(int a,int b)
+{
+  System.out.println("Assignment 1.1 second option");
+	 ans=a-(-b);
+   System.out.println("ANS is " +ans);
+}
+//Assignment 1.2
+void display()
+{
+	  System.out.println("Assignment 1.2");
+	b=(a--)-(--a);
+	System.out.println("int b= a-- - --a output is - " +b);
+	c=a--;
+	System.out.println("int c=a-- output is - " +c);
+	 d=a>>2;	
+	System.out.println("int d=a>>2;output is   - "+d);
+	 e=a&b;
+	System.out.println("int e=a&b output is - " +e);
+}
+//Assignment 1.3
+void swap(int x,int y)
+{
+	System.out.println("Assignment 1.3");
+	System.out.println("Before Swap");
+	System.out.println("x value "+x);
+	System.out.println("y value "+y);
+	 x = x+y;
+     y=x-y;
+     x=x-y;
+	System.out.println("After Swap");
+	System.out.println("x value "+x);
+	System.out.println("y value "+y);
+}
+//Assignment 1.4
+void Ascii()
+{
+	System.out.println("Assignment 1.4");
+	for(asc=65; asc<=90; asc++)
+    {
+        ch = new Character((char)asc).toString();
+        System.out.print(asc+ "-" + ch + "\n");
+    }
+
+}
+}
+
---
+++ TestFirstAsm .java
@@ -0,0 +1,19 @@
+
+public class TestFirstAsm {
+
+	public static void main(String[] args) {
+		FirstAsm f=new FirstAsm();
+		{
+	    //Assignment 1.1
+		f.add(10,20);
+		f.add1(10,20);
+		//Assignment 1.2
+		f.display();
+		//Assignment 1.3
+		f.swap(100,200);
+		//Assignment 1.4
+		f.Ascii();
+		}
+	}
+
+}
